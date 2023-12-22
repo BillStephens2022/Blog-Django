8c2346963b33ee64c74c9beb1ac454494be915c0
BillStephens2022 <two4onebill@yahoo.com>
2023-12-22 15:35:27 -0500

    updated all-posts and post-detail page, added app screenshots to readme

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 ## Description
 A blog website built with Django.  This is a work in process.
+
+### App Screenshots
+
+#### Home Page
+![Home Page](./blog/static/blog/images/screenshot1.png)
+
+#### All Posts
+![All Posts Page](./blog/static/blog/images/screenshot2.png)
--- blog/views.py
+++ blog/views.py
@@ -29,8 +29,8 @@ all_posts = [
     {
         "slug": "programming-is-fun",
         "image": "coding.jpg",
-        "author": "Maximilian",
-        "date": date(2022, 3, 10),
+        "author": "Bill",
+        "date": date(2023, 11, 30),
         "title": "Programming Is Great!",
         "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
         "content": """
@@ -50,8 +50,8 @@ all_posts = [
     {
         "slug": "into-the-woods",
         "image": "woods.jpg",
-        "author": "Maximilian",
-        "date": date(2020, 8, 5),
+        "author": "Bill",
+        "date": date(2023, 8, 5),
         "title": "Nature At Its Best",
         "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
         "content": """
@@ -86,8 +86,13 @@ def starting_page(request):
 
 
 def posts(request):
-    return render(request, "blog/all-posts.html")
+    return render(request, "blog/all-posts.html", {
+        "all_posts": all_posts
+    })
 
 
 def post_detail(request, slug):
-    return render(request, "blog/post-detail.html")
+    selected_post = next(post for post in all_posts if post['slug'] == slug)
+    return render(request, "blog/post-detail.html", {
+        "post": selected_post
+    })
