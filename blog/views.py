643c1ab49f7fc6b8517ed59be6b8412e7d4521d4
BillStephens2022 <two4onebill@yahoo.com>
2023-12-22 14:27:00 -0500

    temporarily added dummy post data to set up rendering multiple posts

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,85 @@
+from datetime import date
 from django.shortcuts import render
 
+posts = [
+    {
+        "slug": "hike-in-the-mountains",
+        "image": "mountains.jpg",
+        "author": "Bill",
+        "date": date(2023, 12, 22),
+        "title": "Mountain Hiking",
+        "excerpt": "There's nothing like the views when hiking in the mountains!  And I wasn't even prepared for what happened whilst I was enjoying the view!",
+        "content": """
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Ut sit iusto, repellat corrupti rerum natus neque perspiciatis 
+            veniam in harum quis veritatis similique aliquam illo, suscipit 
+            at sint dolor officiis!
+
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque 
+            odit recusandae id veritatis. Totam assumenda a beatae porro et, 
+            ad, minus quam provident unde in animi ab asperiores corporis 
+            facilis.
+
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Perspiciatis excepturi delectus iure dolore, facere laboriosam 
+            mollitia! Ullam labore ducimus rem laborum neque minus, ratione 
+            voluptate! Aperiam cum in odio quidem.
+            """
+    },
+    {
+        "slug": "programming-is-fun",
+        "image": "coding.jpg",
+        "author": "Maximilian",
+        "date": date(2022, 3, 10),
+        "title": "Programming Is Great!",
+        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    },
+    {
+        "slug": "into-the-woods",
+        "image": "woods.jpg",
+        "author": "Maximilian",
+        "date": date(2020, 8, 5),
+        "title": "Nature At Its Best",
+        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    }
+]
+
 # Create your views here.
+
+
 def starting_page(request):
     return render(request, "blog/index.html")
 
+
 def posts(request):
     return render(request, "blog/all-posts.html")
+
 
 def post_detail(request, slug):
     return render(request, "blog/post-detail.html")
